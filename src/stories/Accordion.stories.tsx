daa992388269d29b326d302787fa21c3e5032295
AntonChislov <antonchislov6642@gmail.com>
2023-01-05 22:50:06 +0300

    10 storybook update

--- src/stories/Accordion.stories.tsx
+++ src/stories/Accordion.stories.tsx
@@ -1,18 +1,37 @@
 import React, {useState} from 'react';
-import {Accordion} from "../components/Accordion";
+import {Accordion, AccordionPropsType} from "../components/Accordion";
 import {action} from "@storybook/addon-actions";
+import {Story} from "@storybook/react";
 
 export default {
     title: 'Accordion',
     component: Accordion
 }
 
-
-export const AccordionCollapsed = () => <Accordion title={'Menu'} collapsed={true} liList={['1', '2', '3']} setCollapsed={action('wont uncollapsed')}/>
-
-export const AccordionUnCollapsed = () => <Accordion title={'Menu'} collapsed={false} liList={['1', '2', '3']} setCollapsed={action('wont collapsed')}/>
-
-export const AccordionChange = () => {
-const [collapsed, setCollapsed] = useState<boolean>(false)
-    return <Accordion title={'Menu'} collapsed={collapsed} liList={['1', '2', '3']} setCollapsed={setCollapsed}/>
+const Template: Story<AccordionPropsType> = (args) => <Accordion {...args}/>
+export const AccordionCollapsed = Template.bind({})
+AccordionCollapsed.args = {
+    title: 'Menu',
+    collapsed: true,
+    setCollapsed: action('wont uncollapsed'),
+    liList: ['1', '2', '3']
 }
+
+export const AccordionUnCollapsed = Template.bind({})
+AccordionUnCollapsed.args = {
+    title: 'User',
+    collapsed: false,
+    liList: ['1', '2', '3'],
+    setCollapsed: action('wont collapsed')
+}
+
+//storybook виснет при отрисовке этого кейса
+
+/*export const AccordionChanged: Story<AccordionPropsType> = (args) => {
+    const [collapsed, setCollapsed] = useState<boolean>(false)
+    return <AccordionChanged {...args} collapsed={collapsed} setCollapsed={setCollapsed}/>
+}
+AccordionChanged.args = {
+    title: 'User',
+    liList: ['1', '2', '3']
+}*/
--- src/stories/OnOff.stories.tsx
+++ src/stories/OnOff.stories.tsx
@@ -1,17 +1,22 @@
 import React, {useState} from 'react';
-import {OnOff} from "../components/OnOff";
+import {OnOff, OnOffPropsType} from "../components/OnOff";
 import {action} from "@storybook/addon-actions";
+import {Story} from "@storybook/react";
 
 export default {
     title: 'OnOff',
     component: OnOff
 }
 
-const callback = action('wont change')
+/*export const OnOffChanging: Story<OnOffPropsType> = (args) => {
+    const [on, setOn] = useState(true)
+    return <OnOff {...args} setOn={setOn} on={on}/>
+}*/
 
-export const OffMode = () => <OnOff on={false} setOn={callback}/>
-export const OnMode = () => <OnOff on={true} setOn={callback}/>
-export const OnOffChange = () => {
-    const [on, setOn] = useState<boolean>(false)
-    return <OnOff on={on} setOn={setOn}/>
+const Template: Story<OnOffPropsType> = (args) => <OnOff {...args}/>
+
+export const OnOffChanging = Template.bind({})
+OnOffChanging.args = {
+    on: true,
+    setOn: action('changing')
 }
